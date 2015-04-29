c9933c6bd9bb2679d43a4adac476d3c8c1d383dd
Charly Koza <cka@f4-group.com>
2015-04-29 14:59:32 +0200

    also enable postgis / sfcgal on given db

--- postgis-template.sh
+++ postgis-template.sh
@@ -7,9 +7,16 @@ CREATE DATABASE template_postgis TEMPLATE template0 ENCODING 'UTF8';
 UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
 EOSQL
 
+#http://stackoverflow.com/questions/27547933/how-to-create-postgis-extension-for-postgresql-in-docker
 POSTGIS_CONFIG=/usr/share/postgresql/$PG_MAJOR/contrib/postgis-$POSTGIS_MAJOR
 #postgres does not allow CREATE EXTENSION in single user mode
 gosu postgres postgres --single -j template_postgis < $POSTGIS_CONFIG/postgis.sql
 gosu postgres postgres --single -j template_postgis < $POSTGIS_CONFIG/topology.sql
 gosu postgres postgres --single -j template_postgis < $POSTGIS_CONFIG/spatial_ref_sys.sql
 gosu postgres postgres --single -j template_postgis < $POSTGIS_CONFIG/sfcgal.sql
+
+#also enable it on given POSTGRES_DB (default to postgres)
+gosu postgres postgres --single -j $POSTGRES_DB < $POSTGIS_CONFIG/postgis.sql
+gosu postgres postgres --single -j $POSTGRES_DB < $POSTGIS_CONFIG/topology.sql
+gosu postgres postgres --single -j $POSTGRES_DB < $POSTGIS_CONFIG/spatial_ref_sys.sql
+gosu postgres postgres --single -j $POSTGRES_DB < $POSTGIS_CONFIG/sfcgal.sql
