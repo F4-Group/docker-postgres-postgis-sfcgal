d2693ab7658f77f15926291b54ad660b2deb5681
Charly Koza <cka@f4-group.com>
2015-04-29 12:25:13 +0200

    use postgres in single mode

--- postgis-template.sh
+++ postgis-template.sh
@@ -1,10 +1,15 @@
 #!/bin/zsh -e
 echo "Creating template postgis"
 gosu postgres createdb template_postgis -E UTF8 -T template0
+
 echo "Enabling template_postgis as a template"
-gosu postgres postgres -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';"
-echo "enabling postgis"
-gosu postgres postgres -d template_postgis -c "CREATE EXTENSION postgis;"
-gosu postgres postgres -d template_postgis -c "CREATE EXTENSION postgis_topology;"
-echo "Loading sfcgal.sql"
-gosu postgres postgres -d template_postgis -f /scripts/sfcgal.sql
+gosu postgres postgres --single -jE <<EOSQL
+UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
+EOSQL
+
+gosu postgres postgres --single -jE template_postgis <<EOSQL
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+EOSQL
+
+gosu postgres postgres --single -jE template_postgis << /scripts/sfcgal.sql
