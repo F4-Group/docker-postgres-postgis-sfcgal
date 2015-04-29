d49016fd824a7c4f93e219d44cfa9c3bd699d51a
Charly Koza <cka@f4-group.com>
2015-04-29 14:38:03 +0200

    line delimiter allowed as end of statement

--- postgis-template.sh
+++ postgis-template.sh
@@ -4,13 +4,13 @@ gosu postgres postgres --single -E <<EOSQL
 CREATE DATABASE template_postgis TEMPLATE template0 ENCODING 'UTF8';
 EOSQL
 
-gosu postgres postgres --single -jE <<EOSQL
+gosu postgres postgres --single -E <<EOSQL
 UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
 EOSQL
 
-gosu postgres postgres --single -jE template_postgis <<EOSQL
+gosu postgres postgres --single -E template_postgis <<EOSQL
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
 EOSQL
 
-gosu postgres postgres --single -jE template_postgis << /scripts/sfcgal.sql
+gosu postgres postgres --single -E template_postgis << /scripts/sfcgal.sql
