8eddc847ce3a4148f250d079da00b562f0da310b
Charly Koza <cka@f4-group.com>
2015-10-22 17:04:14 +0200

    removed obsolete comments

--- postgis-template.sh
+++ postgis-template.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 # file is sourced from a bash script so shebang is not used
-#inspired by https://github.com/appropriate/docker-postgis/blob/master/9.4-2.1/initdb-postgis.sh
 
 echo "Creating template_postgis as a template"
 psql --username "$POSTGRES_USER" <<- EOSQL
@@ -8,7 +7,6 @@ CREATE DATABASE template_postgis TEMPLATE template0 ENCODING 'UTF8';
 UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
 EOSQL
 
-#http://stackoverflow.com/questions/27547933/how-to-create-postgis-extension-for-postgresql-in-docker
 psql --username "$POSTGRES_USER" --dbname "template_postgis" <<- 'EOSQL'
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
