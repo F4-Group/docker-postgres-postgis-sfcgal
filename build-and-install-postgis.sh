4270f4a681c1d5b3d79e130df273ea97bb2c1d07
Jazz <jazzzz@gmail.com>
2017-10-13 17:54:29 +0200

    Extract build and install dir variables

--- build-and-install-postgis.sh
+++ build-and-install-postgis.sh
@@ -3,6 +3,8 @@
 set -e
 
 PROCESSOR_COUNT=$(nproc)
+BUILD_DIR=/install-postgis
+INSTALL_DIR=/usr/local
 
 #from http://postgis.net/source
 POSTGIS=http://download.osgeo.org/postgis/source/postgis-$POSTGIS_VERSION.tar.gz
@@ -18,62 +20,62 @@ CGAL=https://github.com/CGAL/cgal/archive/releases/CGAL-$CGAL_VERSION.tar.gz
 #from https://github.com/Oslandia/SFCGAL/releases
 SFCGAL=https://github.com/Oslandia/SFCGAL/archive/v$SFCGAL_VERSION.tar.gz
 
-mkdir -p /install-postgis/geos
-cd /install-postgis/geos
-wget -O /install-postgis/geos.tar.bz2 $GEOS
-tar xf /install-postgis/geos.tar.bz2 -C /install-postgis/geos --strip-components=1
-./configure --disable-shared --enable-static && make -j $PROCESSOR_COUNT && make install
+mkdir -p $BUILD_DIR/geos
+cd $BUILD_DIR/geos
+wget -O $BUILD_DIR/geos.tar.bz2 $GEOS
+tar xf $BUILD_DIR/geos.tar.bz2 -C $BUILD_DIR/geos --strip-components=1
+./configure --prefix=$INSTALL_DIR --disable-shared --enable-static && make -j $PROCESSOR_COUNT && make install
 ldconfig
-cd /install-postgis
+cd $BUILD_DIR
 test -x geos
 
-mkdir -p /install-postgis/gdal
-cd /install-postgis/gdal
-wget -O /install-postgis/gdal.tar.gz $GDAL
-tar xf /install-postgis/gdal.tar.gz -C /install-postgis/gdal --strip-components=1
-./configure --disable-shared --enable-static --with-geos=/usr/local/bin/geos-config && make -j $PROCESSOR_COUNT && make install
+mkdir -p $BUILD_DIR/gdal
+cd $BUILD_DIR/gdal
+wget -O $BUILD_DIR/gdal.tar.gz $GDAL
+tar xf $BUILD_DIR/gdal.tar.gz -C $BUILD_DIR/gdal --strip-components=1
+./configure --prefix=$INSTALL_DIR --disable-shared --enable-static --with-geos=$INSTALL_DIR/bin/geos-config && make -j $PROCESSOR_COUNT && make install
 ldconfig
-cd /install-postgis
+cd $BUILD_DIR
 test -x gdal
 
-mkdir -p /install-postgis/proj
-cd /install-postgis/proj
-wget -O /install-postgis/proj.tar.gz $PROJ
-tar xf /install-postgis/proj.tar.gz -C /install-postgis/proj --strip-components=1
-./configure --disable-shared --enable-static && make -j $PROCESSOR_COUNT && make install
-cd /install-postgis
-test -f /usr/local/include/proj_api.h
+mkdir -p $BUILD_DIR/proj
+cd $BUILD_DIR/proj
+wget -O $BUILD_DIR/proj.tar.gz $PROJ
+tar xf $BUILD_DIR/proj.tar.gz -C $BUILD_DIR/proj --strip-components=1
+./configure --prefix=$INSTALL_DIR --disable-shared --enable-static && make -j $PROCESSOR_COUNT && make install
+cd $BUILD_DIR
+test -f $INSTALL_DIR/include/proj_api.h
 
-mkdir -p /install-postgis/cgal
-cd /install-postgis/cgal
-wget -O /install-postgis/cgal.tar.xz $CGAL
-tar xf /install-postgis/cgal.tar.xz -C /install-postgis/cgal --strip-components=1
+mkdir -p $BUILD_DIR/cgal
+cd $BUILD_DIR/cgal
+wget -O $BUILD_DIR/cgal.tar.xz $CGAL
+tar xf $BUILD_DIR/cgal.tar.xz -C $BUILD_DIR/cgal --strip-components=1
 cmake -DBUILD_SHARED_LIBS=OFF . && make -j $PROCESSOR_COUNT && make install
-cd /install-postgis
-test -d /usr/local/lib/CGAL
+cd $BUILD_DIR
+test -d $INSTALL_DIR/lib/CGAL
 
-sfcgal_config=/usr/local/bin/sfcgal-config
-mkdir -p /install-postgis/sfcgal
-cd /install-postgis/sfcgal
-wget -O /install-postgis/sfcgal.tar.gz $SFCGAL
-tar xf /install-postgis/sfcgal.tar.gz -C /install-postgis/sfcgal --strip-components=1
+sfcgal_config=$INSTALL_DIR/bin/sfcgal-config
+mkdir -p $BUILD_DIR/sfcgal
+cd $BUILD_DIR/sfcgal
+wget -O $BUILD_DIR/sfcgal.tar.gz $SFCGAL
+tar xf $BUILD_DIR/sfcgal.tar.gz -C $BUILD_DIR/sfcgal --strip-components=1
 cmake -DSFCGAL_USE_STATIC_LIBS=ON . && make -j $PROCESSOR_COUNT && make install
-cd /install-postgis
+cd $BUILD_DIR
 test -x $sfcgal_config
 
-mkdir -p /install-postgis/postgis
-cd /install-postgis/postgis
-wget -O /install-postgis/postgis.tar.gz $POSTGIS
-tar xf /install-postgis/postgis.tar.gz -C /install-postgis/postgis --strip-components=1
-./configure --with-geosconfig=/usr/local/bin/geos-config --with-gdalconfig=/usr/local/bin/gdal-config --with-sfcgal=$sfcgal_config --with-projdir=/usr/local --with-raster --with-topology && make
-cd /install-postgis/postgis/extensions/postgis
+mkdir -p $BUILD_DIR/postgis
+cd $BUILD_DIR/postgis
+wget -O $BUILD_DIR/postgis.tar.gz $POSTGIS
+tar xf $BUILD_DIR/postgis.tar.gz -C $BUILD_DIR/postgis --strip-components=1
+./configure --with-geosconfig=$INSTALL_DIR/bin/geos-config --with-gdalconfig=$INSTALL_DIR/bin/gdal-config --with-sfcgal=$sfcgal_config --with-projdir=$INSTALL_DIR --with-raster --with-topology && make
+cd $BUILD_DIR/postgis/extensions/postgis
 make -j $PROCESSOR_COUNT && make install
-cd /install-postgis/postgis/extensions/postgis_topology
+cd $BUILD_DIR/postgis/extensions/postgis_topology
 make -j $PROCESSOR_COUNT && make install
-cd /install-postgis/postgis
+cd $BUILD_DIR/postgis
 make install
-cd /install-postgis
+cd $BUILD_DIR
 ldconfig
 
 cd /
-rm -rf /install-postgis
+rm -rf $BUILD_DIR
