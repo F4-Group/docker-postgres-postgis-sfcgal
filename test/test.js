01540de137a86929d6d224bf8a6e517957de3d0a
Charly Koza <cka@f4-group.com>
2019-04-16 12:25:43 +0200

    Check for 3857 projection instead of 900913

--- test/test.js
+++ test/test.js
@@ -8,6 +8,6 @@ test().catch(err => {
 
 async function test() {
     const db = pgp(`postgres://postgres:@postgres:5432/template_postgis?ssl=false&application_name=autotests`);
-    const srid900913projections = await db.any('SELECT * FROM spatial_ref_sys WHERE srid = $1', [900913]);
-    assert.strictEqual(srid900913projections.length, 1, "No 900913 projection when expecting one");
+    const srid3857projections = await db.any('SELECT * FROM spatial_ref_sys WHERE srid = $1', [3857]);
+    assert.strictEqual(srid3857projections.length, 1, "No 3857 projection when expecting one");
 }
